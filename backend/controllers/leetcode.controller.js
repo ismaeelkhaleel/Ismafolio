3dee6177abebbb663d71dda61f5dc9d75d271a99
Mohd Ismaeel <pmohd367@gmail.com>
2025-08-20 09:23:25 +0530

    gfg and leetcode data fetched successfully

--- backend/controllers/leetcode.controller.js
+++ backend/controllers/leetcode.controller.js
@@ -8,81 +8,85 @@ const leetcode = new LeetCode();
 const username = "Mohd_Ismaeel";
 
 const fetchLeetCodeData = async () => {
-  const user = await leetcode.user(username);
-  const existingUser = await LeetcodeState.findOne({
-    username: user.matchedUser.username,
-  });
-
-  let savedUser;
-  if (existingUser) {
-    existingUser.point = user.matchedUser.contributions.points;
-    existingUser.starRating = user.matchedUser.profile.starRating;
-    existingUser.ranking = user.matchedUser.profile.ranking;
-    existingUser.profileUrl = "https://leetcode.com/u/Mohd_Ismaeel/";
-    existingUser.acSubmissionNum =
-      user.matchedUser.submitStats.acSubmissionNum.map((item) => ({
-        difficulty: item.difficulty,
-        count: item.count,
-        submissions: item.submissions,
-      }));
-    savedUser = await existingUser.save();
-  } else {
-    savedUser = new LeetcodeState({
+  try {
+    const user = await leetcode.user(username);
+    const existingUser = await LeetcodeState.findOne({
       username: user.matchedUser.username,
-      point: user.matchedUser.contributions.points,
-      starRating: user.matchedUser.profile.starRating,
-      ranking: user.matchedUser.profile.ranking,
-      profileUrl: "https://leetcode.com/u/Mohd_Ismaeel/",
-      acSubmissionNum: user.matchedUser.submitStats.acSubmissionNum.map(
-        (item) => ({
+    });
+
+    let savedUser;
+    if (existingUser) {
+      existingUser.point = user.matchedUser.contributions.points;
+      existingUser.starRating = user.matchedUser.profile.starRating;
+      existingUser.ranking = user.matchedUser.profile.ranking;
+      existingUser.profileUrl = "https://leetcode.com/u/Mohd_Ismaeel/";
+      existingUser.acSubmissionNum =
+        user.matchedUser.submitStats.acSubmissionNum.map((item) => ({
           difficulty: item.difficulty,
           count: item.count,
           submissions: item.submissions,
-        })
-      ),
-    });
-    savedUser = await savedUser.save();
-  }
-  const recentSolvedProblems = user.recentSubmissionList;
-  for (const problem of recentSolvedProblems) {
-    const existingProblem = await LeetcodeProblem.findOne({
-      title: problem.title,
-    });
-
-    if (problem.statusDisplay !== "Accepted") {
-      continue;
-    }
-
-    const solvedDate = new Date(problem.timestamp * 1000);
-
-    if (!existingProblem) {
-      const newProblem = new LeetcodeProblem({
-        title: problem.title,
-        titleSlug: `https://leetcode.com/problems/${problem.titleSlug}`,
-        solvedOn: solvedDate,
-        status: problem.statusDisplay,
-      });
-      await newProblem.save();
-    }
-  }
-  const parseCalender = JSON.parse(user.matchedUser.submissionCalendar);
-  const submissionCalendar = Object.entries(parseCalender);
-  for (const [timestamp, submissions] of submissionCalendar) {
-    const dateOnly = new Date(parseInt(timestamp) * 1000);
-    dateOnly.setUTCHours(0, 0, 0, 0);
-    const existingSubmission = await LeetcodeHeatmap.findOne({
-      date: dateOnly,
-    });
-    if (existingSubmission) {
-      existingSubmission.submissions = Number(submissions);
-      await existingSubmission.save();
+        }));
+      savedUser = await existingUser.save();
     } else {
-      const newSubmission = new LeetcodeHeatmap({
-        date: dateOnly,
-        submissions: Number(submissions),
+      savedUser = new LeetcodeState({
+        username: user.matchedUser.username,
+        point: user.matchedUser.contributions.points,
+        starRating: user.matchedUser.profile.starRating,
+        ranking: user.matchedUser.profile.ranking,
+        profileUrl: "https://leetcode.com/u/Mohd_Ismaeel/",
+        acSubmissionNum: user.matchedUser.submitStats.acSubmissionNum.map(
+          (item) => ({
+            difficulty: item.difficulty,
+            count: item.count,
+            submissions: item.submissions,
+          })
+        ),
       });
-      await newSubmission.save();
+      savedUser = await savedUser.save();
     }
+    const recentSolvedProblems = user.recentSubmissionList;
+    for (const problem of recentSolvedProblems) {
+      const existingProblem = await LeetcodeProblem.findOne({
+        title: problem.title,
+      });
+
+      if (problem.statusDisplay !== "Accepted") {
+        continue;
+      }
+
+      const solvedDate = new Date(problem.timestamp * 1000);
+
+      if (!existingProblem) {
+        const newProblem = new LeetcodeProblem({
+          title: problem.title,
+          titleSlug: `https://leetcode.com/problems/${problem.titleSlug}`,
+          solvedOn: solvedDate,
+          status: problem.statusDisplay,
+        });
+        await newProblem.save();
+      }
+    }
+    const parseCalender = JSON.parse(user.matchedUser.submissionCalendar);
+    const submissionCalendar = Object.entries(parseCalender);
+    for (const [timestamp, submissions] of submissionCalendar) {
+      const dateOnly = new Date(parseInt(timestamp) * 1000);
+      dateOnly.setUTCHours(0, 0, 0, 0);
+      const existingSubmission = await LeetcodeHeatmap.findOne({
+        date: dateOnly,
+      });
+      if (existingSubmission) {
+        existingSubmission.submissions = Number(submissions);
+        await existingSubmission.save();
+      } else {
+        const newSubmission = new LeetcodeHeatmap({
+          date: dateOnly,
+          submissions: Number(submissions),
+        });
+        await newSubmission.save();
+      }
+    }
+  } catch (err) {
+    console.log(err.message);
   }
 };
 
