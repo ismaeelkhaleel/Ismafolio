d67c432bd41d701e8a02aacc30ceb2b3b287d66f
Mohd Ismaeel <pmohd367@gmail.com>
2025-09-16 07:40:18 +0530

    slug problem solved

--- frontend/src/app/blogs/details/[slug]/page.jsx
+++ frontend/src/app/blogs/details/[slug]/page.jsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 
 export default function BlogDetailPage() {
   const { getBlogDetail, blogDetail } = useUser();
-  const {  } = useParams();
+  const { slug } = useParams();
   const blogId = slug.split("-")[0];
 
   useEffect(() => {
