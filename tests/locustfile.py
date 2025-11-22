1b731de05f7ae5fff8496c04347591848b80f944
Mohd Ismaeel <pmohd367@gmail.com>
2025-11-22 16:23:51 +0530

    improve initial loading

--- tests/locustfile.py
+++ tests/locustfile.py
@@ -5,49 +5,49 @@ class ApiUser(HttpUser):
 
     @task
     def get_profile(self):
-        self.client.get("/get-profile")
+        self.client.get("https://api.mohdismaeel.me/get-profile")
 
     @task
     def get_blogs(self):
-        self.client.get("/get-blogs")
+        self.client.get("https://api.mohdismaeel.me/get-blogs")
 
     @task
     def get_education(self):
-        self.client.get("/get-education")
+        self.client.get("https://api.mohdismaeel.me/get-education")
 
     @task
     def get_experience(self):
-        self.client.get("/get-experience")
+        self.client.get("https://api.mohdismaeel.me/get-experience")
 
     @task
     def get_projects(self):
-        self.client.get("/get-projects")
+        self.client.get("https://api.mohdismaeel.me/get-projects")
 
     @task
     def get_skills(self):
-        self.client.get("/get-skills")
+        self.client.get("https://api.mohdismaeel.me/get-skills")
 
     @task
     def get_leetcode_state(self):
-        self.client.get("/get-leetcode-state")
+        self.client.get("https://api.mohdismaeel.me/get-leetcode-state")
 
     @task
     def get_leetcode_problems(self):
-        self.client.get("/get-leetcode-problems")
+        self.client.get("https://api.mohdismaeel.me/get-leetcode-problems")
 
     @task
     def get_leetcode_heatmap(self):
-        self.client.get("/get-leetcode-heatmap")
+        self.client.get("https://api.mohdismaeel.me/get-leetcode-heatmap")
 
     @task
     def get_gfg_stats(self):
-        self.client.get("/get-gfg-stats")
+        self.client.get("https://api.mohdismaeel.me/get-gfg-stats")
 
     @task
     def get_gfg_problems(self):
-        self.client.get("/get-gfg-problems")
+        self.client.get("https://api.mohdismaeel.me/get-gfg-problems")
 
     @task
     def get_blog_detail(self):
-        self.client.get("/get-blog-detail/68bf7d7d012c05c12fdc8623")
+        self.client.get("https://api.mohdismaeel.me/get-blog-detail/68bf7d7d012c05c12fdc8623")
 
