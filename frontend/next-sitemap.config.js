37ce893e568066f1b5badff6b4b2616dd81c04d1
Mohd Ismaeel <pmohd367@gmail.com>
2026-03-16 16:21:58 +0530

    sitemap.xml generated

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://yourwebsite.com',
+  siteUrl: "https://mohdismaeel.me",
   generateRobotsTxt: true,
-}
+
+  additionalPaths: async (config) => [
+    await config.transform(config, "/"),
+    await config.transform(config, "/blogs"),
+    await config.transform(config, "/projects"),
+    await config.transform(config, "/terminal"),
+    await config.transform(config, "/geeksforgeek/problems"),
+    await config.transform(config, "/leetcode/problems"),
+  ],
+};
